38479f178e3e57746ec558b277b6840ab888f8a1
SchitnikovAS <aleksej.baxman@bk.ru>
2024-07-01 20:44:34 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+works_completed = 12
+time = 1.5
+course = 'Python'
+time_one = time/works_completed
+print(f'Курс: {course}, всего задач:{works_completed}, затрачено часов: {time}, среднее время выполнения {time_one} часа')
